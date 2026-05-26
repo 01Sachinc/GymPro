29b6869a836b12c8d5fd44b2dcc99bafc5edc2d2
Sachin Somannachari <102504524+01Sachinc@users.noreply.github.com>
2026-05-26 11:34:39 +0530

    Update SignupRequest.java

--- gympro-backend/src/main/java/com/gympro/dto/request/SignupRequest.java
+++ gympro-backend/src/main/java/com/gympro/dto/request/SignupRequest.java
@@ -1,25 +1,44 @@
-package com.gympro.dto.request;
+package com.gympro.dto;
 
-import com.gympro.entity.enums.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class SignupRequest {
-    @NotBlank
+
     private String name;
 
-    @NotBlank
-    @Email
     private String email;
 
-    @NotBlank
     private String password;
 
     private String phone;
 
-    private Role role; // Added for testing purposes; typically members wouldn't set this themselves
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
