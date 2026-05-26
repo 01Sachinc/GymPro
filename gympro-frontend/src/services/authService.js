af82f1ab67c3c8f7e8d4442062b2d849724fc55a
Sachin Somannachari <102504524+01Sachinc@users.noreply.github.com>
2026-05-26 11:35:36 +0530

    Update authService.js

--- gympro-frontend/src/services/authService.js
+++ gympro-frontend/src/services/authService.js
@@ -1,35 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = "http://13.206.120.195:8080/api/auth";
 
-const register = (name, email, password, phone) => {
-    return axios.post(API_URL + 'signup', {
-        name,
-        email,
-        password,
-        phone,
-    });
+export const signup = async (userData) => {
+
+    return axios.post(
+        `${API_URL}/signup`,
+        userData,
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
 };
-
-const login = async (email, password) => {
-    const response = await axios.post(API_URL + 'signin', {
-        email,
-        password,
-    });
-    if (response.data.token) {
-        localStorage.setItem('user', JSON.stringify(response.data));
-    }
-    return response.data;
-};
-
-const logout = () => {
-    localStorage.removeItem('user');
-};
-
-const authService = {
-    register,
-    login,
-    logout,
-};
-
-export default authService;
